c5e823baccb9864b2cba1e3f00f0a3fbc5922974
beendra <alexandra.ricardel@gmail.com>
2021-04-15 21:02:10 -0500

    adding

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
-# code here!
+class School
+
+    attr_accessor :name, :roster
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(students_name, grade)
+        roster[grade] ||= []
+        roster[grade] << students_name
+    end
+
+    def grade(students_grade)
+        roster[students_grade]
+    end
+
+
+end
+
+@school = School.new("JHS")
+
+school.add_student("Alex", 12)
+school.roster
